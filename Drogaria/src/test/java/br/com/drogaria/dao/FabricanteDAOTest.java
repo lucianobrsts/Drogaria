5d98f10294fac6f0c0b935a8e53d6c0b9a8b9087
Luciano Brito <Luciano Brito@192.168.15.8>
2018-09-27 00:45:15 -0300

    Implementado todos os métodos de teste da entidade Fabricante

--- Drogaria/src/test/java/br/com/drogaria/dao/FabricanteDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/FabricanteDAOTest.java
@@ -1,5 +1,56 @@
 package br.com.drogaria.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.com.drogaria.domain.Fabricante;
+
 public class FabricanteDAOTest {
+	@Test
+	@Ignore
+	public void salvar() {
+		Fabricante fabricante = new Fabricante();
+		fabricante.setDescricao("Safoni");
+
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.salvar(fabricante);
+	}
+
+	@Test
+	@Ignore
+	@SuppressWarnings("unused")
+	public void listar() {
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		List<Fabricante> resultado = fabricanteDAO.listar();
+	}
+
+	@Test
+	@Ignore
+	public void buscar() {
+		Long codigo = 3L;
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		fabricanteDAO.buscar(codigo);
+	}
+
+	@Test
+	@Ignore
+	public void excluir() {
+		Long codigo = 3L;
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		Fabricante fabricante = fabricanteDAO.buscar(codigo);
+		fabricanteDAO.excluir(fabricante);
+	}
+
+	@Test
+	@Ignore
+	public void editar() {
+		Long codigo = 4L;
+		FabricanteDAO fabricanteDAO = new FabricanteDAO();
+		Fabricante fabricante = fabricanteDAO.buscar(codigo);
+		fabricante.setDescricao("Safones");
+		fabricanteDAO.editar(fabricante);
+	}
 
 }
