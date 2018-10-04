7ba3cfb12f50ebf58903cb287d184ddcfc1ea486
Luciano Brito <Luciano Brito@192.168.15.2>
2018-10-03 21:18:42 -0300

    Finalizada classe UsuarioDAO e seus devidos testes.

--- Drogaria/src/test/java/br/com/drogaria/dao/UsuarioDAOTest.java
+++ Drogaria/src/test/java/br/com/drogaria/dao/UsuarioDAOTest.java
@@ -1,5 +1,66 @@
 package br.com.drogaria.dao;
 
-public class UsuarioDAOTest {
+import java.util.List;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.com.drogaria.domain.Pessoa;
+import br.com.drogaria.domain.Usuario;
+
+public class UsuarioDAOTest {
+	@Test
+	@Ignore
+	public void salvar() {
+		Usuario usuario = new Usuario();
+		usuario.setSenha("1234");
+		usuario.setTipo('A');
+		usuario.setAtivo(true);
+
+		Long codigo = 1L;
+		PessoaDAO pessoaDAO = new PessoaDAO();
+		Pessoa pessoa = pessoaDAO.buscar(codigo);
+		usuario.setPessoa(pessoa);
+
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		usuarioDAO.salvar(usuario);
+
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	@Ignore
+	public void listar() {
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		List<Usuario> resultado = usuarioDAO.listar();
+
+	}
+
+	@SuppressWarnings("unused")
+	@Test
+	@Ignore
+	public void buscar() {
+		Long codigo = 1L;
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscar(codigo);
+	}
+
+	@Test
+	@Ignore
+	public void excluir() {
+		Long codigo = 1L;
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscar(codigo);
+		usuarioDAO.excluir(usuario);
+	}
+
+	@Test
+	@Ignore
+	public void editar() {
+		Long codigo = 1L;
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscar(codigo);
+		usuario.setSenha("123456");
+		usuarioDAO.editar(usuario);
+	}
 }
